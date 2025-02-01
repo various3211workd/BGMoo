e964bc1332c5b06a43f1b5e03aa775bbdf87d02d
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2025-02-02 06:48:51 +0900

    [Add] URLがない場合は window.location.href を設定するように修正

--- entrypoints/content/footer.tsx
+++ entrypoints/content/footer.tsx
@@ -1,14 +1,12 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Button } from "@/components/ui/button";
-import { Play, Pause, Volume2, Plus, Save, X } from "lucide-react";
+import { Play, Pause, Volume2, Music } from "lucide-react";
 import { Slider } from "@/components/ui/slider";
 
 const Footer: React.FC<{
-  onAddReference: (reference: { name: string; url: string }) => void;
   musicSamples: any;
   nowStartText: string;
-}> = ({ onAddReference, musicSamples, nowStartText }) => {
-  const [isAdding, setIsAdding] = useState(false);
+}> = ({ musicSamples, nowStartText }) => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentTime, setCurrentTime] = useState(0);
   const [duration, setDuration] = useState(0);
@@ -61,98 +59,6 @@ const Footer: React.FC<{
   }, [nowStartText, musicSamples, trackCache]);
 
   useEffect(() => {
-    const handleMouseOver = (event: MouseEvent) => {
-      if (!isAdding) return;
-      const target = event.target as HTMLElement;
-      target.style.outline = "2px solid rgba(166, 241, 224, 1)";
-      target.style.backgroundColor = "rgba(115, 199, 199, 0.2)";
-
-      target.addEventListener(
-        "mouseout",
-        () => {
-          target.style.outline = "";
-          target.style.backgroundColor = "";
-        },
-        { once: true }
-      );
-    };
-
-    const handleElementClick = (event: MouseEvent) => {
-      if (!isAdding) return;
-      event.preventDefault();
-      event.stopPropagation();
-      const target = event.target as HTMLElement;
-
-      const path: string[] = [];
-      let currentElement: HTMLElement | null = target;
-
-      while (currentElement) {
-        let selector = currentElement.tagName.toLowerCase();
-        //const tagName = currentElement.tagName.toLowerCase();
-        const className = currentElement.className;
-
-        const id = currentElement.id;
-        if (id) {
-          selector += `#${id}`;
-        } else if (className) {
-          selector += `.${className.split(" ").join(".")}`;
-        }
-        path.unshift(selector);
-        currentElement = currentElement.parentElement;
-      }
-
-      let href = "";
-      if (target.querySelectorAll) {
-        const aElements = target.querySelectorAll("a");
-        aElements.forEach((a) => {
-          if (a.href) {
-            href = a.href;
-          }
-        });
-      }
-
-      const reference = {
-        name: target.innerText.slice(0, 16),
-        url: href,
-        path: path,
-      };
-      onAddReference(reference);
-      setIsAdding(false);
-    };
-
-    if (isAdding) {
-      const elements = document.querySelectorAll("a, button");
-
-      elements.forEach((element) => {
-        element.style.pointerEvents = "none";
-      });
-
-      setTimeout(() => {
-        document.addEventListener("mouseover", handleMouseOver);
-        document.addEventListener("click", handleElementClick);
-      }, 500);
-    } else {
-      const elements = document.querySelectorAll("a, button");
-
-      elements.forEach((element) => {
-        element.style.pointerEvents = "auto";
-      });
-
-      document.removeEventListener("mouseover", handleMouseOver);
-      document.removeEventListener("click", handleElementClick);
-    }
-
-    return () => {
-      document.removeEventListener("mouseover", handleMouseOver);
-      document.removeEventListener("click", handleElementClick);
-    };
-  }, [isAdding, onAddReference]);
-
-  const handleAddClick = () => {
-    setIsAdding(!isAdding);
-  };
-
-  useEffect(() => {
     if (audioRef.current) {
       audioRef.current.volume = volume;
     }
@@ -202,10 +108,13 @@ const Footer: React.FC<{
   return (
     <footer className="absolute bottom-0 w-full flex my-4">
       <hr />
-      <div className="flex items-center flex-col">
+      <div className="flex items-center w-full flex-col">
         {musicSamples && (
           <div className="flex justify-center items-center">
-            <div className="text-sm font-medium mx-4">
+            <div>
+              <Music className="h-4 w-4" />
+            </div>
+            <div className="text-sm font-medium mr-4">
               {currentTrack ? currentTrack.title : "No Track"}
             </div>
             <div className="text-xs text-muted-foreground">
@@ -213,7 +122,7 @@ const Footer: React.FC<{
             </div>
           </div>
         )}
-        <div className="flex items-center justify-between space-x-2">
+        <div className="flex w-full items-center justify-between space-x-2">
           {musicSamples && (
             <>
               <audio
@@ -249,29 +158,6 @@ const Footer: React.FC<{
               </div>
             </>
           )}
-          <div className="flex items-center space-x-2">
-            <Button
-              id="add-button"
-              className={`font-bold py-2 px-4 rounded 
-              ${
-                isAdding
-                  ? "bg-red-500 hover:bg-red-700"
-                  : "bg-blue-500 hover:bg-blue-700 text-4xl"
-              }`}
-              onClick={handleAddClick}
-              variant="ghost"
-              size="icon"
-            >
-              {isAdding ? (
-                <X className="h-4 w-4" />
-              ) : (
-                <Plus className="h-4 w-4" />
-              )}
-            </Button>
-            <Button variant="ghost" size="icon">
-              <Save className="h-4 w-4" />
-            </Button>
-          </div>
         </div>
       </div>
     </footer>
