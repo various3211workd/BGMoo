08e24ecfab605ebfd6d0f1334cb8ae12de6003fa
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2025-02-03 20:36:17 +0900

    [Add] default thema

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,34 +1,34 @@
 // ThemeContext.js
-import React, {createContext, useState, useContext, useEffect} from 'react';
+import React, { createContext, useState, useContext, useEffect } from "react";
 
-const ThemeContext = createContext<{ theme: string, toggleTheme: Function }>({
-    theme: 'light', toggleTheme: (theme: string) => {
-    }
+const ThemeContext = createContext<{ theme: string; toggleTheme: Function }>({
+  theme: "default",
+  toggleTheme: (theme: string) => {},
 });
 
 export const useTheme = () => useContext(ThemeContext);
 
-export const ThemeProvider = ({children}: { children: any }) => {
-    const [theme, setTheme] = useState('light');
+export const ThemeProvider = ({ children }: { children: any }) => {
+  const [theme, setTheme] = useState("default");
 
-    const toggleTheme = (theme: string) => {
-        setTheme(theme);
-    };
+  const toggleTheme = (theme: string) => {
+    setTheme(theme);
+  };
 
-    async function initTheme() {
-        let data = await browser.storage.local.get('theme');
-        if (data.theme) {
-            setTheme(data.theme)
-        }
+  async function initTheme() {
+    let data = await browser.storage.local.get("theme");
+    if (data.theme) {
+      setTheme(data.theme);
     }
+  }
 
-    useEffect(() => {
-        initTheme();
-    }, []);
+  useEffect(() => {
+    initTheme();
+  }, []);
 
-    return (
-        <ThemeContext.Provider value={{theme, toggleTheme}}>
-            {children}
-        </ThemeContext.Provider>
-    );
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 };
