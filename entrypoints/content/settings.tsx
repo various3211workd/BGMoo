1466abb728dd714964920efb8c64675df595aaf4
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2025-02-04 09:35:13 +0900

    [Add] settingにgeminiAPIに自動で送信する設定を追加

--- entrypoints/content/settings.tsx
+++ entrypoints/content/settings.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import {I18nSettings} from "@/components/settings/i18n-settings.tsx";
-import {ThemeSettings} from "@/components/settings/theme-settings.tsx";
+import { I18nSettings } from "@/components/settings/i18n-settings.tsx";
+import { ThemeSettings } from "@/components/settings/theme-settings.tsx";
+import { AuteUseGeiniAPISettings } from "@/components/settings/auteUseGeminiAPI-settings";
 
 export function SettingsPage() {
-    return (
-        <div className="grid gap-4">
-            <I18nSettings/>
-            <ThemeSettings/>
-        </div>
-    )
+  return (
+    <div className="grid gap-4">
+      <I18nSettings />
+      <ThemeSettings />
+      <AuteUseGeiniAPISettings />
+    </div>
+  );
 }
-
